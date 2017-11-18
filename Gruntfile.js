13b1d0fbfb3389390c1e8d8c36a1075e54982deb
DigiContributor <esotericizm88@gmail.com>
2017-11-18 11:52:17 +1000

    digibytego

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,3 @@
-var bitcoreTasks = require('bitcore-build');
 module.exports = function(grunt) {
 
   // Project configuration.
@@ -6,8 +5,6 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json')
   });
 
-  bitcoreTasks('wallet-client');
-  console.log(bitcoreTasks());
   // Default task(s).
-  grunt.registerTask('default', ['browser']);
+  grunt.registerTask('default', []);
 };
