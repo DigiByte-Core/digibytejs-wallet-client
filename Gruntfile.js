98c97e751b55514de5499a993b2e4c260d39ce8c
digicontributer <esotericizm@digibyte.io>
2018-01-29 18:54:09 +1000

    update dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,9 @@
-var bitcoreTasks = require('bitcore-build');
 module.exports = function(grunt) {
-
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json')
   });
 
-  bitcoreTasks('wallet-client');
-  console.log(bitcoreTasks());
   // Default task(s).
-  grunt.registerTask('default', ['browser']);
+  grunt.registerTask('default', []);
 };
